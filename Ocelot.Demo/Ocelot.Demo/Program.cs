ff761c86a037d2ce22791208d4327279f023a881
Obi Oberoi <obi@eOberoi.com>
2022-02-12 17:46:54 -0500

    added ocelot.middleware using directive

--- Ocelot.Demo/Ocelot.Demo/Program.cs
+++ Ocelot.Demo/Ocelot.Demo/Program.cs
@@ -1,4 +1,5 @@
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,5 +26,6 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 app.UseHttpLogging();
+app.UseOcelot().Wait();
 
 app.Run();
