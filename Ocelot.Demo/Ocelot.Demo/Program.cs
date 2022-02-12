760026594ac8c32943a28d5352236a6e0b99927c
Obi Oberoi <obi@eOberoi.com>
2022-02-12 17:44:22 -0500

    added the using directive

--- Ocelot.Demo/Ocelot.Demo/Program.cs
+++ Ocelot.Demo/Ocelot.Demo/Program.cs
@@ -1,7 +1,10 @@
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
