b0ef53b13ad10cd64f18ffee8eee39c890506db8
Obi Oberoi <obi@eOberoi.com>
2022-02-20 22:24:15 -0500

    added a method to return cities

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
@@ -5,5 +5,14 @@ namespace Ocelot.Demo.Api2.Controllers
     [ApiController]
     public class CitiesController : ControllerBase
     {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<Object> {
+                    new { id = 1, Name = "Toronto"},
+                    new { id = 2, Name = "New Delhi"},
+                    new { id = 3, Name = "Dallas"}
+                });
+        }
     }
 }
