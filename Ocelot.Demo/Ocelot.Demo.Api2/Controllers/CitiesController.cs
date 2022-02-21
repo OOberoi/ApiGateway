1d0a8d9e0d65183424460c5294082120b52e5525
Obi Oberoi <obi@eOberoi.com>
2022-02-21 14:26:26 -0500

    declared routing at the controller level

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
@@ -3,16 +3,18 @@
 namespace Ocelot.Demo.Api2.Controllers
 {
     [ApiController]
+    [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
-        [HttpGet("api/cities")]   
+        //HttpGet("api/cities")]   
         public JsonResult GetCities()
         {
             return new JsonResult(
                 new List<Object> {
                     new { id = 1, Name = "Toronto"},
                     new { id = 2, Name = "New Delhi"},
-                    new { id = 3, Name = "Dallas"}
+                    new { id = 3, Name = "Dallas"},
+                    new { id = 4, Name = "Melbourne"}
                 });
         }
     }
