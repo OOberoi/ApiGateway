5397940a3009da911c32ae1a1d283a880d74e099
Obi Oberoi <obi@eOberoi.com>
2022-02-22 20:49:02 -0500

    added a constructor and initialized data

--- Ocelot.Demo/Ocelot.Demo.Api2/CitiesDataStore.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/CitiesDataStore.cs
@@ -6,5 +6,45 @@ namespace Ocelot.Demo.Api2
     {
         public List<CityDto> Cities { get; set; }
 
+        public CitiesDataStore()
+        {
+            // data initialization
+            Cities = new List<CityDto>()
+            { 
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Toronto",
+                    Description = "City known for CN Tower"                
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Mumbai",
+                    Description = "City known for Bollywood"
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "New Delhi",
+                    Description = "City known for rich culture and vibrancy"
+                },
+                new CityDto()
+                {
+                    Id = 4,
+                    Name = "New York",
+                    Description = "City that doesn't sleep"
+                },
+                new CityDto()
+                {
+                    Id = 5,
+                    Name = "Melbourne",
+                    Description = "City known for four seasons in a day"
+                },
+
+
+            };
+        }
+
     }
 }
