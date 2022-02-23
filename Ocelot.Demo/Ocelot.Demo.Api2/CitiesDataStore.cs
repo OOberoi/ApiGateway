169d1a4d49cf46e8f0e77aaef6843264839e70a4
Obi Oberoi <obi@eOberoi.com>
2022-02-22 20:37:29 -0500

    added a prop

--- Ocelot.Demo/Ocelot.Demo.Api2/CitiesDataStore.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/CitiesDataStore.cs
@@ -1,6 +1,10 @@
-﻿namespace Ocelot.Demo.Api2
+﻿using Ocelot.Demo.Api2.Models;
+
+namespace Ocelot.Demo.Api2
 {
     public class CitiesDataStore
     {
+        public List<CityDto> Cities { get; set; }
+
     }
 }
