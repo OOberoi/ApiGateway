7af2c335dc236dd2b54545054427b3d044d1e73a
Obi Oberoi <obi@eOberoi.com>
2022-02-23 22:15:16 -0500

    added a POI collection to the cities dto

--- Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ocelot.Demo.Api2.Models
-{
-    public class PointOfInterest
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-    }
-}
