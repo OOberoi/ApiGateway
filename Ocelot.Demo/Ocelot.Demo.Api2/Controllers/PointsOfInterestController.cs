1b713ba376d8c3504d23972e1dbd71a87094df8d
Obi Oberoi <obi@eOberoi.com>
2022-02-23 22:42:36 -0500

    modified the route attribute

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/PointsOfInterestController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/PointsOfInterestController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Ocelot.Demo.Api2.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/cities/{cityId}/pointsofinterest")]
     [ApiController]
     public class PointsOfInterestController : ControllerBase
     {
