2bee03ad4aa506c40fbd813990cc6896b98c5fd2
Obi Oberoi <obi@eOberoi.com>
2022-02-28 22:32:57 -0500

    added the route attribute

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/FilesController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/FilesController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Ocelot.Demo.Api2.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[files]")]
     [ApiController]
     public class FilesController : ControllerBase
     {
