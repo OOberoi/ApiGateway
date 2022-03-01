ba7dde95f175c61d155b6c269bd585afc542c46e
Obi Oberoi <obi@eOberoi.com>
2022-02-28 22:32:22 -0500

    added files controller

---
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/FilesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ocelot.Demo.Api2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+    }
+}
