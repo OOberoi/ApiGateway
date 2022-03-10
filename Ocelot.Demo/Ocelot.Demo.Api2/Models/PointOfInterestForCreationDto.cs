cfef15a81f8ce0f9305a3f2478a0509a260bc46f
Obi Oberoi <obi@eOberoi.com>
2022-03-09 22:26:57 -0500

    added data validations

--- Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForCreationDto.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForCreationDto.cs
@@ -4,6 +4,7 @@ namespace Ocelot.Demo.Api2.Models
 {
     public class PointOfInterestForCreationDto
     {
+        [Required ]
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
 
