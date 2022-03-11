245d9ae964816c8b564b053de612aa99e862b0c2
Obi Oberoi <obi@eOberoi.com>
2022-03-10 22:41:53 -0500

    added couple properties

--- Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForUpdateDto.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForUpdateDto.cs
@@ -4,6 +4,8 @@ namespace Ocelot.Demo.Api2.Models
 {
     public class PointOfInterestForUpdateDto
     {
+        public string Name { get; set; }
 
+        public string? Description { get; set; }
     }
 }
