15178a2e206ba2851e861f52551e3a9d59a97bb2
Obi Oberoi <obi@eOberoi.com>
2022-03-10 21:49:41 -0500

    slight change

--- Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForCreationDto.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForCreationDto.cs
@@ -4,7 +4,7 @@ namespace Ocelot.Demo.Api2.Models
 {
     public class PointOfInterestForCreationDto
     {
-        [Required(ErrorMessage = "Name is missing!") ]
+        [Required(ErrorMessage = "Name is missing!") ] 
         [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
 
