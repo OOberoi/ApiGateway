5dab24675dbca53122bd817f039618748141621f
Obi Oberoi <obi@eOberoi.com>
2022-03-10 22:43:22 -0500

    added validation rules

--- Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForUpdateDto.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForUpdateDto.cs
@@ -4,8 +4,11 @@ namespace Ocelot.Demo.Api2.Models
 {
     public class PointOfInterestForUpdateDto
     {
+        [Required(ErrorMessage = "Name should be provided")]
+        [MaxLength(50)]
         public string Name { get; set; }
 
+        [MaxLength(250)]
         public string? Description { get; set; }
     }
 }
