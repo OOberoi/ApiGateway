ccb7a8f6f59c20e24a0c58f98c2fe5c3af55614a
Obi Oberoi <obi@eOberoi.com>
2022-03-16 23:12:06 -0400

    added logging to the middleware

--- Ocelot.Demo/Ocelot.Demo.Api2/Program.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.StaticFiles;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
 
 // Add services to the container.
 builder.Services.AddControllers(options =>
