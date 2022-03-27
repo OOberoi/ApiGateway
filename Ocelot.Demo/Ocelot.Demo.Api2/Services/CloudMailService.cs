56f5d608e4d17adea99e6ef8570989c831c4fa66
Obi Oberoi <obi@eOberoi.com>
2022-03-26 20:47:53 -0400

    added the implementation

--- Ocelot.Demo/Ocelot.Demo.Api2/Services/CloudMailService.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Services/CloudMailService.cs
@@ -1,6 +1,16 @@
 ﻿namespace Ocelot.Demo.Api2.Services
 {
-    public class CloudMailService
+    public class CloudMailService : IMailService
     {
+        private string _mailTo = "obi@eOberoi.com";
+        public string _mailFrom = "noreply@eOberoi.com";
+
+        public void Send(string subject, string message)
+        {
+            Console.WriteLine($"Mail from {_mailTo} to {_mailTo}, with {nameof(CloudMailService)}.");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Message: {message}");
+        }
+
     }
 }
