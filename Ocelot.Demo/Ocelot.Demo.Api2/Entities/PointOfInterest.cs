24fcc5f376fa4ee8523ddc65f1b3a5ddb73662ed
Obi Oberoi <obi@eOberoi.com>
2022-03-31 22:45:46 -0400

    added identity attribute and the usings

--- Ocelot.Demo/Ocelot.Demo.Api2/Entities/PointOfInterest.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Entities/PointOfInterest.cs
@@ -1,11 +1,12 @@
-﻿
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ocelot.Demo.Api2.Entities
 {
     public class PointOfInterest
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
 
