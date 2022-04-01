fe48c33ae679634e4897f715511f9eaae6a245e6
Obi Oberoi <obi@eOberoi.com>
2022-03-31 22:33:42 -0400

    added key attribute

--- Ocelot.Demo/Ocelot.Demo.Api2/Entities/City.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Entities/City.cs
@@ -1,9 +1,10 @@
-﻿//using Ocelot.Demo.Api2.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Ocelot.Demo.Api2.Entities
 {
     public class City
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
