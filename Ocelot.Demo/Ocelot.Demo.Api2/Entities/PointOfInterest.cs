71ede2ca88e752248e164f83eaf6d877293c326e
Obi Oberoi <obi@eOberoi.com>
2022-03-31 22:44:27 -0400

    added key attribute

--- Ocelot.Demo/Ocelot.Demo.Api2/Entities/PointOfInterest.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Entities/PointOfInterest.cs
@@ -1,7 +1,11 @@
-﻿namespace Ocelot.Demo.Api2.Entities
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Ocelot.Demo.Api2.Entities
 {
     public class PointOfInterest
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
 
