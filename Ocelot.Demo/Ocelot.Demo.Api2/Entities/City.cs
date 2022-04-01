57d71b75540e1e0310388a6ceff573b6721407b2
Obi Oberoi <obi@eOberoi.com>
2022-03-31 22:27:50 -0400

    added props

--- Ocelot.Demo/Ocelot.Demo.Api2/Entities/City.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Entities/City.cs
@@ -1,9 +1,13 @@
-﻿namespace Ocelot.Demo.Api2.Entities
+﻿//using Ocelot.Demo.Api2.Models;
+
+namespace Ocelot.Demo.Api2.Entities
 {
     public class City
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
+
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; } = new List<PointOfInterest>();
     }
 }
