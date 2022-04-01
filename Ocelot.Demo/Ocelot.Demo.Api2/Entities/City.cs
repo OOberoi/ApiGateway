a99789451566570cf0a32b0c3d1032f53e5bec58
Obi Oberoi <obi@eOberoi.com>
2022-03-31 22:36:31 -0400

    added dbgeneration option for identity

--- Ocelot.Demo/Ocelot.Demo.Api2/Entities/City.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Entities/City.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ocelot.Demo.Api2.Entities
 {
     public class City
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
