fa6fb749b4dcd2b5509c69f4cdf322de9342a3bf
Obi Oberoi <obi@eOberoi.com>
2022-04-02 21:25:37 -0400

    added a bunch of attributes for validation

--- Ocelot.Demo/Ocelot.Demo.Api2/Entities/PointOfInterest.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Entities/PointOfInterest.cs
@@ -8,6 +8,7 @@ namespace Ocelot.Demo.Api2.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [MaxLength(50)]
         public string Name { get; set; }
         [ForeignKey("CityId")]
         public City? City { get; set; }
