5a4deca207f157db4aa33db56ad41d88b5ffeb6a
Obi Oberoi <obi@eoberoi.com>
2022-04-06 10:15:39 -0400

    minor change

--- Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForUpdateDto.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForUpdateDto.cs
@@ -4,7 +4,7 @@ namespace Ocelot.Demo.Api2.Models
 {
     public class PointOfInterestForUpdateDto
     {
-        [Required(ErrorMessage = "Name should be provided")]
+        [Required(ErrorMessage = "Name should be provided!")]
         [MaxLength(50)]
         public string? Name { get; set; }
 
