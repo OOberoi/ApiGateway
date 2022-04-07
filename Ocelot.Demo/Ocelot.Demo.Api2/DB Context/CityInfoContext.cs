d3f038c8b1bbfbaab95b0d872bbe7d1eb991c0e8
Obi Oberoi <obi@eoberoi.com>
2022-04-07 02:49:10 -0400

    added the DbSets of entities

--- Ocelot.Demo/Ocelot.Demo.Api2/DB Context/CityInfoContext.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/DB Context/CityInfoContext.cs
@@ -6,5 +6,6 @@ namespace Ocelot.Demo.Api2.DB_Context
     public class CityInfoContext : DbContext
     {
         public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointOfInterests { get; set;}
     }
 }
