589869be9fa763599f2bb1bb10de17547dd71ebd
Obi Oberoi <obi@eoberoi.com>
2022-04-07 02:53:45 -0400

    added the null forgiveable operator

--- Ocelot.Demo/Ocelot.Demo.Api2/DB Context/CityInfoContext.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/DB Context/CityInfoContext.cs
@@ -6,6 +6,6 @@ namespace Ocelot.Demo.Api2.DB_Context
     public class CityInfoContext : DbContext
     {
         public DbSet<City> Cities { get; set; } = null!;
-        public DbSet<PointOfInterest> PointOfInterests { get; set;}
+        public DbSet<PointOfInterest> PointOfInterests { get; set;} = null!;
     }
 }
