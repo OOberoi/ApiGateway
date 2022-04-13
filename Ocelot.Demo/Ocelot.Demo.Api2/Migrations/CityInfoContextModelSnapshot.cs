32a02ce1d66ef8315253d88e363e330790f40ca7
Obi Oberoi <obi@eoberoi.com>
2022-04-13 08:01:27 -0400

    added a new migration

--- Ocelot.Demo/Ocelot.Demo.Api2/Migrations/CityInfoContextModelSnapshot.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Migrations/CityInfoContextModelSnapshot.cs
@@ -41,6 +41,38 @@ namespace Ocelot.Demo.Api2.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Cities");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Description = "City known for CN Tower",
+                            Name = "Toronto"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Description = "City known for Bollywood",
+                            Name = "Mumbai"
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Description = "City known for rich culture and diversity",
+                            Name = "Delhi"
+                        },
+                        new
+                        {
+                            Id = 4,
+                            Description = "Fashion and Financial capital",
+                            Name = "New York"
+                        },
+                        new
+                        {
+                            Id = 5,
+                            Description = "Fashion capital",
+                            Name = "Paris"
+                        });
                 });
 
             modelBuilder.Entity("Ocelot.Demo.Api2.Entities.PointOfInterest", b =>
@@ -69,6 +101,64 @@ namespace Ocelot.Demo.Api2.Migrations
                     b.HasIndex("CityId");
 
                     b.ToTable("PointOfInterests");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            CityId = 1,
+                            Description = "An incredible tower",
+                            Name = "CN Tower"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            CityId = 1,
+                            Description = "A city surrounded by the lake",
+                            Name = "Lake Ontario"
+                        },
+                        new
+                        {
+                            Id = 3,
+                            CityId = 2,
+                            Description = "An iconic hotel and pride of India",
+                            Name = "Taj Hotel"
+                        },
+                        new
+                        {
+                            Id = 4,
+                            CityId = 3,
+                            Description = "A historic monument",
+                            Name = "Qutub Minar"
+                        },
+                        new
+                        {
+                            Id = 5,
+                            CityId = 4,
+                            Description = "Iconic towers to replace twin towers",
+                            Name = "One World"
+                        },
+                        new
+                        {
+                            Id = 6,
+                            CityId = 4,
+                            Description = "An extraordinary Art Museum",
+                            Name = "Guggenheim Museum"
+                        },
+                        new
+                        {
+                            Id = 7,
+                            CityId = 5,
+                            Description = "An iconic French tower",
+                            Name = "Eifel Tower"
+                        },
+                        new
+                        {
+                            Id = 8,
+                            CityId = 5,
+                            Description = "Attest the greatness of many civilizations",
+                            Name = "The Louvre"
+                        });
                 });
 
             modelBuilder.Entity("Ocelot.Demo.Api2.Entities.PointOfInterest", b =>
