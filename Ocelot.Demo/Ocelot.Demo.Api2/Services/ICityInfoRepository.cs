85a99a0562ec6f716c3439b488f37d81fd76c900
Obi Oberoi <obi@eoberoi.com>
2022-04-16 06:39:33 -0400

    added an IEnumerable

--- Ocelot.Demo/Ocelot.Demo.Api2/Services/ICityInfoRepository.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Services/ICityInfoRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Ocelot.Demo.Api2.Services
+﻿using Ocelot.Demo.Api2.Entities;
+
+namespace Ocelot.Demo.Api2.Services
+
 {
     public interface ICityInfoRepository
     {
+        IEnumerable<City>GetCities();
+
     }
 }
