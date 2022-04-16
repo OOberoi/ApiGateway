de0ee7c61438ce036ea32d409562dafee49c19b7
Obi Oberoi <obi@eoberoi.com>
2022-04-16 06:52:26 -0400

    added another async method

--- Ocelot.Demo/Ocelot.Demo.Api2/Services/ICityInfoRepository.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Services/ICityInfoRepository.cs
@@ -7,6 +7,6 @@ namespace Ocelot.Demo.Api2.Services
     {
         Task<IEnumerable<City>>GetCitiesAsync();
         Task<City?> GetCityAsync(int cityId);
-
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
     }
 }
