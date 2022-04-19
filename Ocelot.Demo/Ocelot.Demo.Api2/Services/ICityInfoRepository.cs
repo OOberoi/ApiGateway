c0be40b5529d1065a96ade9911a435a0889c6fa1
Obi Oberoi <obi@eoberoi.com>
2022-04-18 22:47:47 -0400

    added a new city repo class

--- Ocelot.Demo/Ocelot.Demo.Api2/Services/ICityInfoRepository.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Services/ICityInfoRepository.cs
@@ -8,7 +8,6 @@ namespace Ocelot.Demo.Api2.Services
         Task<IEnumerable<City>>GetCitiesAsync();
         Task<City?> GetCityAsync(int cityId);
         Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
-
         Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
     }
 }
