d4b9ee0a33f4aa4107f67cf24671a2a766d33962
Obi Oberoi <obi@eoberoi.com>
2022-04-18 22:49:16 -0400

    added implementation

--- Ocelot.Demo/Ocelot.Demo.Api2/Services/CityInfoRepository.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Services/CityInfoRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace Ocelot.Demo.Api2.Services
+﻿using Ocelot.Demo.Api2.Entities;
+
+namespace Ocelot.Demo.Api2.Services
 {
-    public class CityInfoRepository
+    public class CityInfoRepository : ICityInfoRepository
     {
+        public Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<City?> GetCityAsync(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
