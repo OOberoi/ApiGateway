be8cc645d7109e77725edd39e56161f721853550
Obi Oberoi <obi@eoberoi.com>
2022-04-23 21:03:12 -0400

    added a mapper

--- Ocelot.Demo/Ocelot.Demo.Api2/Profiles/CityProfile.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Profiles/CityProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Ocelot.Demo.Api2.Models;
+using Ocelot.Demo.Api2.Entities;
 
 namespace Ocelot.Demo.Api2.Profiles
 {
@@ -6,7 +8,7 @@ namespace Ocelot.Demo.Api2.Profiles
     {
         public CityProfile()
         {
-
+            CreateMap<City, CityWithoutPointsOfInterestDto>();
         }
     }
 }
