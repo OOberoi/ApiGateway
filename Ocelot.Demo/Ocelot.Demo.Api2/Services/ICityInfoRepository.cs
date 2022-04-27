47cbd7dc09d9ae0855f6d596c8ce4941b116f23d
Obi Oberoi <obi@eOberoi.com>
2022-04-26 21:58:34 -0400

    added a method signature to the interface

--- Ocelot.Demo/Ocelot.Demo.Api2/Services/ICityInfoRepository.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Services/ICityInfoRepository.cs
@@ -5,6 +5,7 @@ namespace Ocelot.Demo.Api2.Services
     public interface ICityInfoRepository
     {
         Task<IEnumerable<City>>GetCitiesAsync();
+        Task<bool> CityExistsAsync();
         Task<City?> GetCityAsync(int cityId, bool includesPointsOfInterest);
         Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
         Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
