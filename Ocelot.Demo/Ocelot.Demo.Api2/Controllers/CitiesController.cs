88a20ed01d7eb8548410788f0fe3a507161dc060
Obi Oberoi <obi@eoberoi.com>
2022-04-28 22:00:30 -0400

    added a comment

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
@@ -36,6 +36,8 @@ namespace Ocelot.Demo.Api2.Controllers
                 return NotFound();                
             }
 
+            // If PointsOfInterest have to included, then map to CityDto, else map to 
+            // CityWithoutPointsOfInterestDto
             if (includePointsOfInterest)
             { 
                 return Ok(_mapper.Map<CityDto>(city));
