1f99c887272cd06a1480b59a6a3b38e07153ddc5
Obi Oberoi <obi@eOberoi.com>
2022-05-07 17:46:28 -0400

    added a searchQuery param

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
@@ -22,7 +22,7 @@ namespace Ocelot.Demo.Api2.Controllers
         
         [HttpGet]
         //[FromQuery] is optional and is typically used for reading purposes. It's not required!
-        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCitiesAsync([FromQuery] string? name)
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCitiesAsync([FromQuery] string? name, string? searchQuery)
         {
             var cityEntities = await _cityInfoRepository.GetCitiesAsync(name);
             return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities)); 
