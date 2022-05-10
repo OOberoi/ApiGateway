00361f6cbb8e3b019d061bdd643b385be3862cd0
Obi Oberoi <obi@eOberoi.com>
2022-05-09 22:39:48 -0400

    adding props

--- Ocelot.Demo/Ocelot.Demo.Api2/Services/PaginationMetadata.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Services/PaginationMetadata.cs
@@ -5,6 +5,6 @@
         public int TotalItemCount { get; set; }
         public int TotalPageCount { get; set; }
         public int PageSize { get; set; }
-
+        public int CurrentPage { get; set; }
     }
 }
