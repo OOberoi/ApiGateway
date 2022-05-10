9d957622b3260f8e47e4472604c8c715c80055d5
Obi Oberoi <obi@eOberoi.com>
2022-05-09 21:28:32 -0400

    minor code changes

--- Ocelot.Demo/Ocelot.Demo.Api2/Services/CityInfoRepository.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Services/CityInfoRepository.cs
@@ -16,12 +16,12 @@ namespace Ocelot.Demo.Api2.Services
         {
             return await _cityInfoContext.Cities.OrderBy(c => c.Name).ToListAsync();
         }
-
+                
         public async Task<IEnumerable<City>> GetCitiesAsync(string? name, string? searchQuery)
         {
             if (string.IsNullOrEmpty(name) && string.IsNullOrWhiteSpace(searchQuery))
             { 
-                return await GetCitiesAsync();
+                return await GetCitiesAsync(); 
             }
 
             var col = _cityInfoContext.Cities as IQueryable<City>;
