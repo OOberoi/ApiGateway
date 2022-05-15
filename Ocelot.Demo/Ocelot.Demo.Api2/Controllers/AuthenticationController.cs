f1260c30d5841aa83e28ec7313292f0da3485527
Obi Oberoi <obi@eOberoi.com>
2022-05-14 21:28:06 -0400

    moved the class outside the controller class

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/AuthenticationController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/AuthenticationController.cs
@@ -7,26 +7,31 @@ namespace Ocelot.Demo.Api2.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
-        public class AuthenticationRequestBody
-        {
-            public string? UserName { get; set; }
-            public string? Password { get; set; }
-       }
+        //public class AuthenticationRequestBody
+        //{
+        //    public string? UserName { get; set; }
+        //    public string? Password { get; set; }
+        //}
 
 
         [HttpPost("authenticate")]
         public ActionResult<string> Authenticate(AuthenticationRequestBody authenticationRequestBody)
         {
             // validate user credentials
-            var user = ValidateUserCredentials();
+            var user = ValidateUserCredentials(authenticationRequestBody.UserName, authenticationRequestBody.Password);
 
 
         
         }
 
-        private object ValidateUserCredentials(string userName, string password)
+        private object ValidateUserCredentials(string? userName, string? password)
         {
             throw new NotImplementedException();
         }
+    }
+    public class AuthenticationRequestBody
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
     }
 }
