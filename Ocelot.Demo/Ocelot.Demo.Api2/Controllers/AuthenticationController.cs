e51710b021f50c192d6bc90374832cec22a1e473
Obi Oberoi <obi@eOberoi.com>
2022-05-14 21:56:55 -0400

    added props

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/AuthenticationController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/AuthenticationController.cs
@@ -19,6 +19,7 @@ namespace Ocelot.Demo.Api2.Controllers
             public string UserName { get; set; }
             public string FirstName { get; set; }
             public string LastName { get; set; }
+            public string City { get; set; }
         }
 
 
