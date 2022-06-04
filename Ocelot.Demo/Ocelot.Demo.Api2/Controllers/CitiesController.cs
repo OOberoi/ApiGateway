9b9b5711a05673c90d6a08f6cec1760a0f062c7d
Obi Oberoi <obi@eOberoi.com>
2022-06-04 17:10:59 -0400

    added response type

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/CitiesController.cs
@@ -39,6 +39,8 @@ namespace Ocelot.Demo.Api2.Controllers
         
         [HttpGet]
         //[FromQuery] is optional and is typically used for reading purposes. It's not a required a!
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
         public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCitiesAsync(
             [FromQuery] string? name, string? searchQuery, int pageNum = 1, int pageSize=10)
         {
