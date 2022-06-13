6ae321eba7f2e830f5073fa5dc7bac00c1b8e025
Obi Oberoi <obi@eOberoi.com>
2022-06-12 23:10:57 -0400

    added xml comments

--- Ocelot.Demo/Ocelot.Demo.Api2/Controllers/PointsOfInterestController.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Controllers/PointsOfInterestController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace Ocelot.Demo.Api2.Controllers
 {
     /// <summary>
-    /// 
+    /// API Project
     /// </summary>
     [Route("api/cities/{cityId}/pointsofinterest")]
     [ApiController]
