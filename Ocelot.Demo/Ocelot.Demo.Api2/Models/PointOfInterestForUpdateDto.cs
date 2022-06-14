9604055ac8493ae54b144634d43f109998c4246a
Obi Oberoi <obi@eoberoi.com>
2022-06-13 20:55:28 -0600

    added xml comments

--- Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForUpdateDto.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Models/PointOfInterestForUpdateDto.cs
@@ -2,6 +2,9 @@
 
 namespace Ocelot.Demo.Api2.Models
 {
+    /// <summary>
+    /// DTO to update point of interest
+    /// </summary>
     public class PointOfInterestForUpdateDto
     {
         [Required(ErrorMessage = "Name should be provided!")]
