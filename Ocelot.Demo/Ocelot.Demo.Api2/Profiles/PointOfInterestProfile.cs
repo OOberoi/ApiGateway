2f0dfb49886adeb81c44f9847b9b2f131b666c9f
Obi Oberoi <obi@eoberoi.com>
2022-06-13 21:07:36 -0600

    added xml comments

--- Ocelot.Demo/Ocelot.Demo.Api2/Profiles/PointOfInterestProfile.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Profiles/PointOfInterestProfile.cs
@@ -4,6 +4,9 @@ using Ocelot.Demo.Api2.Models;
 
 namespace Ocelot.Demo.Api2.Profiles
 {
+    /// <summary>
+    /// A PointOfInterestProfile class that defines mappings between DTOs and entities
+    /// </summary>
     public class PointOfInterestProfile : Profile
     {
         public PointOfInterestProfile()
