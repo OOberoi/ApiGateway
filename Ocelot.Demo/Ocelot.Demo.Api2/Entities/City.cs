5c9e9bf7d52c076d4906e1c7649bb4f95df76985
Obi Oberoi <obi@eOberoi.com>
2022-06-16 23:00:36 -0400

    added xml comments

--- Ocelot.Demo/Ocelot.Demo.Api2/Entities/City.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Entities/City.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ocelot.Demo.Api2.Entities
 {
+    /// <summary>
+    /// A class that represents a city object
+    /// </summary>
     public class City
     {
         [Key]
