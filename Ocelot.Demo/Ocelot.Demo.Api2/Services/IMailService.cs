a17c40f6acd667339e609068d9f0e4cff3b7974f
Obi Oberoi <obi@eOberoi.com>
2022-06-24 23:30:33 -0400

    added xml comment

--- Ocelot.Demo/Ocelot.Demo.Api2/Services/IMailService.cs
+++ Ocelot.Demo/Ocelot.Demo.Api2/Services/IMailService.cs
@@ -1,5 +1,8 @@
 ﻿namespace Ocelot.Demo.Api2.Services
 {
+    /// <summary>
+    /// An interface for Mail Service
+    /// </summary>
     public interface IMailService
     {
         void Send(string subject, string message);
